75d83433e1deffee62542af877a8e7dc31a3031c
Tyson <tysoncadenhead@gmail.com>
2014-06-27 14:12:28 -0500

    Syncing with the server

--- assets/js/controllers/ListCtrl.js
+++ assets/js/controllers/ListCtrl.js
@@ -1,20 +1,29 @@
 define(function () {
-    return ['$scope', function($scope) {
+    return ['$scope', '$http', function($scope, $http) {
+
+        $scope.description = '';
+        
+        $scope.itemCheck = function (data) {
+            $http.get('/list/update?id=' + data.id + '&completed=' + ((data.completed) ? 0: 1));
+        };
+
         $scope.addItem = function () {
-            console.log('addItem');
+            $http.get('/list/create?description=' + $scope.description).success(function(data) {
+              $scope.items.push(data);
+            });
         };
-        $scope.removeItems = function () {
-            console.log('removeItems');
+
+        $scope.removeItem = function (data) {
+            $http['delete']('/list/' + data.id).success(function() {
+              $scope.items.splice(data.index, 1);
+            });
         };
-        $scope.items = [{
-            description: 'Do a thing',
-            completed: true
-        }, {
-            description: 'Do another thing',
-            completed: false
-        }, {
-            description: 'Do a third thing',
-            completed: false
-        }];
+
+        $http.get('/list/find').success(function(data) {
+          for (var i = 0; i < data.length; i++) {
+            data[i].index = i;
+          }
+          $scope.items = data;
+        });
     }];
 });
